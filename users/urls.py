9669119efc1ad7df5a5beb4c0bb3de21bb91ac5d
Maxmud <maxmudbobomurodov151@gmail.com>
2025-07-07 19:10:07 +0500

    users

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('', views.my_profile, name='my_profile'),
     path('teacher/', views.teacher_profile, name='teacher_profile'),
